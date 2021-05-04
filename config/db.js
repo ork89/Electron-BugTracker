fc147fb24a032903097ae30bb5598a06066d1ed1
Ori M. K <kosman.ori@gmail.com>
2021-05-04 19:11:52 +0300

    Removed private authentication details

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(
-			'mongodb+srv://OriKo:EnyZdJNhq6TWI36d@cluster0.vepdz.mongodb.net/BugTracker?retryWrites=true&w=majority',
+			'mongodb+srv://<user>:<password>@cluster0.vepdz.mongodb.net/BugTracker?retryWrites=true&w=majority',
 			{
 				useNewUrlParser: true,
 				useCreateIndex: true,
